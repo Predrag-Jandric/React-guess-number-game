3dfeb2b97925bc4b5aba7786c1adf718cb8b8f39
predrag jandric <predrag.jandric.bg@gmail.com>
2023-08-18 23:55:33 +0200

    project complete, logic and styling 100% complete

--- README.md
+++ README.md
@@ -1,70 +1,26 @@
-# Getting Started with Create React App
+# 🛠 React & Tailwind - guess number game
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# 🔗 [Live Preview]()
+![Design preview](./src/assets/preview.gif)
 
-## Available Scripts
+## Welcome! 👋
 
-In the project directory, you can run:
+- I made a game using React where you guess a secret number between 1 and 15. The game helped me practice React logic. Although I followed a tutorial for most of it, I also got to experiment with it myself.
 
-### `npm start`
+---
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+- You enter a number, and the game tells you if it's too high or too low.
 
-### `npm test`
+- The game keeps track of how many times you try to guess. You start with a score of 30 and it goes down each time you guess wrong.
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+- There's a highscore too! It shows your best score, so you can try to beat it.
 
-### `npm run build`
+---
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+### Languages and features 👨‍💻 
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+- React: local state management, conditional rendering, if statements
+- Tailwind: utility classes
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+Thanks for checking out my projects.
--- src/App.js
+++ src/App.js
@@ -1,36 +1,82 @@
 import React from "react";
-import arrow from '../src/arrow-right.svg'
-import wave from '../src/wave.svg'
-
+import wave from '../src//assets/wave.svg'
+import ConfettiExplosion from 'react-confetti-explosion';
 
 function App() {
+  const generateRandomNum = function (min, max) {
+    return Math.trunc(Math.random() * (max - min + 1)) + min;
+  }
+
+  const [secretNum, setSecretNum] = React.useState(generateRandomNum(1, 15));
+  const [inputNum, setInputNum] = React.useState(0);
+  const [message, setMessage] = React.useState("Start guessing...");
+  const [score, setScore] = React.useState(30);
+  const [highScore, setHighScore] = React.useState(0);
+  const [win, setWin] = React.useState(false)
+
+  const [isExploding, setIsExploding] = React.useState(false);
+
+  const checkNumber = () => {
+    if (inputNum === "") {
+      setMessage("Start guessing...");
+    } else if (inputNum > secretNum) {
+      setMessage("❌ Too high");
+      setScore((prev) => prev - 1);
+    } else if (inputNum < secretNum) {
+      setMessage("❌ Too low");
+      setScore((prev) => prev - 1);
+      // eslint-disable-next-line eqeqeq
+    } else if (inputNum == secretNum) {
+      setMessage("✅ Correct !");
+      setWin(true)
+      setIsExploding(true)
+      if (score > highScore) {
+        setHighScore(score);
+      }
+    }
+  };
+
+  const handleReset = () => {
+    setSecretNum(generateRandomNum(1, 15))
+    setInputNum(0)
+    setMessage("Start guessing...")
+    setScore(30)
+    setWin(false)
+    setIsExploding(false)
+  }
+
   return (
-    <div className="text-slate-100 flex flex-col min-h-screen  bg-slate-600">
-
+    <main className="text-slate-100 flex flex-col min-h-screen  bg-slate-600">
       <header className="flex flex-col justify-center text-center bg-slate-700 items-center gap-12 pb-5">
-        <button className="border-2 border-slate-300 text-xl py-3 m-5 px-3 ml-auto hover:bg-slate-100 hover:text-slate-900 font-semibold transition-colors">Reset game</button>
-        <h1 className="text-4xl mx-3">Guess the number in range 1-20</h1>
-        <p className="bg-white text-slate-900 w-[10rem] mx-auto py-4 text-4xl">10</p>
+        <button onClick={handleReset} className={`${win ? "animate-bounce" : ""} border-2 border-slate-300 text-xl py-3 m-5 px-3 ml-auto hover:bg-slate-100 hover:text-slate-900 font-semibold transition-all`}>Reset game</button>
+        <h1 className="text-4xl mx-3">{win ? "The secret number was" : "Guess the number in range 1-15"}<span>{isExploding && <ConfettiExplosion />}</span></h1>
+        <p className={`bg-white text-slate-900 w-[10rem] mx-auto py-4 text-4xl ${win ? "bg-green-300" : ""}`}>{win ? secretNum : "?"}</p>
       </header>
-
 
       <img src={wave} alt="waves transition" />
 
-
-      <main className="flex justify-center items-center pt-10 gap-20 max-[640px]:flex-col max-[640px]:gap-0">
+      <section className="flex justify-center items-center pt-10 gap-20 max-[640px]:flex-col max-[640px]:gap-0">
         <article className="text-2xl max-[640px]:order-last mb-14 max-[640px]:text-center">
-          <p className="-mt-2">Start guessing <img className="max-[640px]:hidden inline-block w-7 h-7" src={arrow} alt="" /></p>
-          <p className="mt-14">Score:</p>
-          <p className="mt-1">Highscore:</p>
+          <p className="-mt-6">{message}</p>
+          <p className="mt-14">Score: {score}</p>
+          <p className="mt-1">Highscore: {highScore}</p>
         </article>
-
-        <article className="flex flex-col w-48 gap-8 mb-14">
-          <input className="border-2 text-center bg-slate-600 h-20 text-5xl border-cyan-300" type="text" />
-          <button className="text-xl py-3 px-3 bg-slate-100 font-semibold text-slate-900 w-[80%] mx-auto transition-colors hover:bg-slate-300">Check !</button>
+        <article className="flex flex-col w-48 gap-8 mb-14 relative">
+          <input
+            onChange={(e) => setInputNum(e.target.value)}
+            value={inputNum}
+            className="border-2 text-center bg-slate-600 h-24 text-5xl border-cyan-300"
+            type="number"
+          />
+          <button
+            onClick={checkNumber}
+            className="text-xl py-3 px-3 bg-slate-100 font-semibold text-slate-900 w-[80%] mx-auto transition-colors hover:bg-slate-400 hover:text-white"
+          >
+            Check !
+          </button>
         </article>
-      </main>
-
-    </div>
+      </section>
+    </main>
   );
 }
 
